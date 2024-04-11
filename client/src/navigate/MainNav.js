c0dfbcca67369150a7fe10c15a7d6e721cf7ddbf
EgorBartuli <EgorBartuli>
2024-04-11 10:06:46 +0200

    optimizacion

--- client/src/navigate/MainNav.js
+++ client/src/navigate/MainNav.js
@@ -5,10 +5,12 @@ import { useSelector } from "react-redux";
 const MainNav = () => {
   const { isAuth, user, status } = useSelector((store) => store.user);
 
-  //Component
   return (
     <header>
-      <nav style={{backgroundColor: "(59,89,152)"}} className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav
+        style={{ backgroundColor: "rgb(59, 89, 152)" }}
+        className="navbar navbar-expand-lg navbar-dark bg-dark"
+      >
         <div className="container-fluid">
           <Link className="navbar-brand px-3" to="/">
             Student-Invest
@@ -26,28 +28,25 @@ const MainNav = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarText">
             <ul className="navbar-nav mb-2 mb-lg-0">
-              {/* IF Auth is false */}
-              {isAuth === false && (
-                <li className="nav-item px-3">
-                  <Link className="classes.nav-link" to="/auth/register">
-                    Sign Up
-                  </Link>
-                </li>
+              {!isAuth && (
+                <>
+                  <li className="nav-item px-3">
+                    <Link className="nav-link" to="/auth/register">
+                      Sign Up
+                    </Link>
+                  </li>
+                  <li className="nav-item px-3">
+                    <Link className="nav-link" to="/auth/login">
+                      Sign In
+                    </Link>
+                  </li>
+                </>
               )}
 
-              {isAuth === false && (
-                <li className="nav-item px-3">
-                  <Link className="nav-link" to="/auth/login">
-                    Sign In
-                  </Link>
-                </li>
-              )}
-
-              {/* IF Auth is true */}
               {isAuth && (
                 <>
                   <li className="nav-item px-3">
-                  <Link className="nav-link" to="/profile">
+                    <Link className="nav-link" to="/profile">
                       Welcome back, {user} | Your status is, {status}
                     </Link>
                   </li>
@@ -72,3 +71,4 @@ const MainNav = () => {
 };
 
 export default MainNav;
+
